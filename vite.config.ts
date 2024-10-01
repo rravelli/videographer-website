68bc8b1ff5813bd281792b454c4a8ace39b5baa4
rravelli <remi.ravelli@gmail.com>
2024-10-01 10:28:45 +0200

    fix: add base URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: { outDir: "build", assetsDir: "static" },
+  base: "https://rravelli.github.io/videographer-website/",
 });
